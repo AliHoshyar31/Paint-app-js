e796081048e66d2c9e668ca6331b2b81e59f81f8
Hoshyar <hoshyar3169@gmail.com>
2024-09-12 01:45:01 +0330

    Brush & Eraser solved 👍

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.querySelector("canvas");
 const brushWidth = document.querySelector("#brush-width")
 const brushColor = document.querySelector("#color-picker")
+const brush = document.querySelector(".brush")
+const eraser = document.querySelector(".eraser")
 const ctx = canvas.getContext("2d")
 
 let isDrawing = false
@@ -31,4 +33,21 @@ brushWidth.addEventListener("change", () => {
 
 brushColor.addEventListener("change", () => {
     ctx.strokeStyle = `${brushColor.value}`
+    if(eraser.className == "eraser active"){
+        brush.classList.add("active");
+        eraser.classList.remove("active");
+        ctx.strokeStyle = `${brushColor.value}`
+    }
+})
+
+brush.addEventListener("click", () => {
+    brush.classList.add("active");
+    eraser.classList.remove("active");
+    ctx.strokeStyle = `${brushColor.value}`
+})
+
+eraser.addEventListener("click", () => {
+    brush.classList.remove("active");
+    eraser.classList.add("active");
+    ctx.strokeStyle = "#fff"
 })
